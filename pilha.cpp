e8c91c204d148977c6fd6ac70f9a08ea091386c1
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-19 18:05:37 -0300

    feit

--- pilha.cpp
+++ pilha.cpp
@@ -4,30 +4,63 @@
 class Pilha{
 
   private: //lista de atributos em privado
-    int topo;
-    int v[MAX];
+    int topo; //psica d ultim element clcad na fila
+    int v[MAX]; //para guardar s valres da pilha
 
   public: //decaracao de metodos
   
     Pilha(){ //contrutor de mesmo nome da classe
       topo = -1; //começa com -1 para que quando seja incrementado va pra 0
     }
+
+    bool pilhaCheia(){
+      return topo ==MAX-1;
+    }
+    bool pilhaVazia(){
+      return topo== -1;
+    }
     
     void empilhar(int n){
+      if(pilhaCheia()){
+        std::cout<<"Pilha cheia!" <<std::endl;
+        return;
+      } //verifica se a pilha esta cheia antes de retirar
       ++topo;
       v[topo] = n;
     }
     
     void desempilhar(){
-      while(topo!= -1){
-        v[topo]=0;
-        --topo;
+      if(pilhaVazia()){
+        std::cout<<"pilha vazia"<<std::endl;
+        return -1;
       }
+      int n =v[topo];
+        // v[topo]=0;
+        --topo;
+      return n;
+    }
+
+    void mostrar(){
+      if(pilhaVazia()){
+        std::cout<<"pilha vazia"<<std::endl;
+        return;
+      }
+      std::cout<<"Pilha"<<std::endl;
+      for(int i=topo; i=0; i--){
+        std::cout<< v[i] <<" ";
+      }
+      std::cout<<std::endl;
     }
     
 };
 
-int main(){
-      Pilha obj1;
-      
-    }
+int main() {
+    Pilha p;
+    p.empilhar(10);
+    p.empilhar(20);
+    p.empilhar(30);
+    p.mostrar();
+
+    std::cout << "Desempilhado: " << p.desempilhar() << std::endl;
+    p.mostrar();
+}
