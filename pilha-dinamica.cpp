d82c9b04eaee0572dab3ee861da418113ecf8644
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-19 18:33:38 -0300

    arrumada pilha-dinamica.cpp

--- pilha-dinamica.cpp
+++ pilha-dinamica.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
+// using namespace std; permite usar cout, cin, endl sem escrever std:: antes
+
+struct Nodo { // definição da estrutura Nodo
+    int info;      // valor armazenado no nó
+    Nodo* prox;    // ponteiro para o próximo nó
+};
 
 class PilhaD{
-    private:
-        Nodo *topo;
-    public:
-        PilhaD();
 
+private:
+    Nodo *topo; // Ponteiro para o topo da pilha
+
+public:
+    PilhaD();
     void empilhar(int n);
     int empilhar();
     void listar();
+};
 
+PilhaD::PilhaD(){
+    topo=nullptr; // inicializa o topo da pilha como nulo
+}
 
-    PilhaD::PilhaD(){
-        topo=nullptr;
-    }
-    novo->info=3; //novo apontando pro campo info recebe 3
-
-    void PilhaD::empilhar(int n){
-        Nodo *novo;
-        novo=new Nodo();
-        novo->info=n; //nao pode fazer isso(se topo esta nulo)!!
+    void PilhaD::empilhar(int n){ // empilha um valor na pilha
+        Nodo *novo=new Nodo();// Cria um novo nó dinamicamente
+        novo->info=n;  // Atribui o valor ao nó //nao pode fazer isso(se topo esta nulo)!!
         if(topo == nullptr){
             nodo->prox=nullptr; //primeiro elemento, o endereco de prox é null
         }
@@ -30,26 +35,24 @@ class PilhaD{
     }
 
     int PilhaD::desempilhar(){
-        Nodo *aux;
-        if(topo!=nullptr){
-            aux=topo;
-            topo=topo->prox; //topo agora é o endereco abaixo
-            int n = aux->info;
-            delete aux;
+        if(topo!=nullptr){ // while tava esvaziand
+            Nodo *aux=topo; // Guarda o topo atual da pilha
+            topo=topo->prox; //topo agora é o prx endereco (no)
+            int n = aux->info; // Armazena o valor desempilhado
+            delete aux;  // Libera a memória do nó desempilhado
             return n;
         }
         else{
-            cout<<"Pilha esta vazia";
+            cout<<"Pilha esta vazia"<<std::endl;
             return -1;
         }
     }
 
     void PilhaD::listar(){
-        Nodo *aux;
-        aux=topo;
-        while(aux !=nullptr){
-            cout<<aux->info<<endl;
-            aux=aux->prox;
+        Nodo *aux=topo; // Começa no topo da pilha
+        if(aux !=nullptr){
+            cout<<aux->info<<endl; // Imprime o valor do nó
+            aux=aux->prox; //vai para prx nó
         }
     }
 
@@ -58,20 +61,11 @@ int main(){
     PilhaD pilha;//instancia do objeto da classe pilha
     pilha.empilhar(4);
     pilha.empilhar(7);
+    pilha.listar();
     pilha.desempilhar();
+    pilha.listar();
+
     int n = pilha.desempilhar();
+    cout << "Valor desempilhado: " << n << endl;
+    return 0;
 }
-
-
-/*
-struct Nodo{
-    int info;
-    struct Nodo *prox;
-};
-
-Nodo *novo;
-novo=new Nodo();
-*/
-
-// jeito menos usual:
-// (*novo).info=3
