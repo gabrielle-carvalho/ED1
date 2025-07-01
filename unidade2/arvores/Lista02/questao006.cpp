c923f3732228ff4a72707ef2c1c44278747fefd5
gabrielle-carvalho <gabiecarvalho15@gmail.com>
2025-07-01 05:02:50 -0300

    feat: questao 06 feita e funcional

---
+++ unidade2/arvores/Lista02/questao006.cpp
@@ -0,0 +1,147 @@
+// 6.  Considere  uma    árvore    binária    de    pesquisa    que    contém    em    cada    nó  as    seguintes 
+// informações relacionadas aos alunos de um determinado curso: o código (inteiro) do aluno e a 
+// média no curso. Escreva uma função, em C, para retirar as folhas da árvore e gerar uma pilha 
+// contendo  apenas  as  folhas  (sem  alocar  nenhum  novo  endereço),  de  tal  forma  que no  topo  da 
+// pilha encontraremos sempre o maior código do aluno.
+
+#include <iostream>
+
+
+struct no {
+    int codigo;
+    float media;
+    no *esq, *dir;
+};
+
+struct Nodo {
+    int info;      
+    Nodo* prox;   
+};
+
+class PilhaD{
+
+private:
+    Nodo *topo; // Ponteiro para o topo da pilha
+
+public:
+    PilhaD();
+    void empilhar(int n);
+    void listar();
+};
+
+class Arvore {
+public:
+    no *raiz;
+    Arvore();
+    no *inserir(no * raiz, int cod, float med);
+    no *retirar(no *raiz, int n);
+    void pilhaFolhas();
+};
+
+Arvore::Arvore() {
+    raiz = nullptr;
+}
+
+no *Arvore::inserir(no *raiz, int cod, float med){
+    if(raiz==nullptr){
+        raiz=new no();
+        raiz->codigo=cod;
+        raiz->media=med;
+        raiz->esq=nullptr;
+        raiz->dir=nullptr;
+        return raiz;
+    }
+
+    if(cod > raiz->codigo){
+        raiz->dir = inserir (raiz->dir, cod, med);
+    }
+    else if(cod<raiz->codigo){
+        raiz->esq = inserir(raiz->esq, cod, med);
+    }
+    else{
+        std::cout<<"Valor repetido";
+        return raiz;
+    }
+    return raiz;
+}
+
+
+// void retirarFolhas(no *&raiz, PilhaD &P) {
+//     if (raiz == nullptr) return;
+
+//     retirarFolhas(raiz->dir, P);
+//     retirarFolhas(raiz->esq, P);
+    
+//     if (raiz->esq == nullptr && raiz->dir == nullptr) {
+//         std::cout << "Removendo folha: " << raiz->codigo << std::endl;
+//         P.empilhar(raiz->codigo);
+//         delete raiz;
+//         raiz = nullptr;
+//     }
+// }
+
+
+void empilharFolhas (no *raiz, PilhaD &P){
+    if(raiz==nullptr){
+        return;
+    }
+    empilharFolhas(raiz->esq, P);
+    empilharFolhas(raiz->dir, P);
+
+    if(raiz->dir==nullptr&&raiz->esq==nullptr){
+        std::cout << "Empilhando folha: " << raiz->codigo << std::endl;
+        P.empilhar(raiz->codigo);
+        return;
+    }
+}
+
+void Arvore::pilhaFolhas(){
+    PilhaD P;
+    empilharFolhas(raiz, P);
+    P.listar();
+}
+
+
+
+PilhaD::PilhaD(){
+    topo=nullptr; // inicializa o topo da pilha como nulo
+}
+
+void PilhaD::empilhar(int n){ // empilha um valor na pilha
+    Nodo *novo=new Nodo();// Cria um novo nó dinamicamente
+    novo->info=n;  // Atribui o valor ao nó //nao pode fazer isso(se topo esta nulo)!!
+    if(topo == nullptr){
+        novo->prox=nullptr; //primeiro elemento, o endereco de prox é null
+    }
+    else{
+        novo->prox=topo; //indica o endereco do prox elemnto da fila
+    }
+    topo=novo;
+}
+
+void PilhaD::listar(){
+    Nodo *aux = topo;
+    while(aux != nullptr){
+        std::cout << aux->info << " ";
+        aux = aux->prox;
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+    Arvore arv;
+    arv.raiz = arv.inserir(arv.raiz, 10, 7.5);
+    arv.raiz = arv.inserir(arv.raiz, 5, 8.0);
+    arv.raiz = arv.inserir(arv.raiz, 3, 9.0);
+    arv.raiz = arv.inserir(arv.raiz, 7, 6.5);
+    arv.raiz = arv.inserir(arv.raiz, 15, 8.5);
+    arv.raiz = arv.inserir(arv.raiz, 12, 5.0);
+    arv.raiz = arv.inserir(arv.raiz, 18, 7.0);
+
+    std::cout << "Chamando pilhaFolhas()\n";
+    arv.pilhaFolhas(); 
+
+    return 0;
+}
+
