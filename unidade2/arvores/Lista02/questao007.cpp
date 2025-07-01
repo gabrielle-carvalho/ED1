354f0d60cbe413180caba45ee99b76fa964c0100
gabrielle-carvalho <gabiecarvalho15@gmail.com>
2025-07-01 11:38:44 -0300

    feat: questao 7, mas sem implemetar tudo

--- unidade2/arvores/Lista02/questao007.cpp
+++ unidade2/arvores/Lista02/questao007.cpp
@@ -1,4 +1,21 @@
-#include <iostream>
+#include<iostream>
+
+struct nodo {
+    int info;
+    nodo *esq, *dir;
+};
+
+class Arvore {
+public:
+    nodo *raiz;
+    Arvore();
+    nodo *inserir(nodo *raiz, int n);
+    nodo *retirar(nodo *raiz, int n);
+    nodo substituir(nodo *raiz, nodo *sucessor);
+    void em_ordem(nodo *raiz);
+    nodo *listaNodos();
+
+};
 
 struct No {
     int info;
@@ -111,8 +128,86 @@ void menu(ListaDupla &lista) {
     }
 }
 
-int main() {
-    ListaDupla lista;
-    menu(lista);
-    return 0;
+Arvore::Arvore() {
+    raiz = nullptr;
+}
+
+nodo *Arvore::inserir(nodo *raiz, int n){
+    if(raiz==nullptr){
+        raiz=new nodo();
+        raiz->info=n;
+        raiz->esq=nullptr;
+        raiz->dir=nullptr;
+        return raiz;
+    }
+    if(n<raiz->info){
+        raiz->dir = inserir(raiz->dir,n);
+    }
+    else if(n>raiz->info){
+        raiz->esq = inserir(raiz->esq, n);
+    }
+    else{
+        std::cout<<"numero igual a um existente";
+        return raiz;
+    }
+    return raiz;
+}
+
+nodo *Arvore::substituir(nodo *raiz, nodo *sucessor){
+    nodo *aux;
+    if(sucessor->esq != nullptr){
+        sucessor->esq = substituir(raiz, sucessor->esq);
+    }
+    else{
+        raiz->info = sucessor->info;
+        aux = sucessor;
+        sucessor = sucessor->dir;
+        delete aux;
+        return sucessor;
+    }
+    return raiz;
+}
+
+void Arvore::em_ordem(nodo *raiz){
+    if(raiz==nullptr){
+        return;
+    }
+    em_ordem(raiz->esq);
+    std::coour<< raiz->info <<std::endl;
+    em_ordem(raiz->dir);
+}
+
+nodo *Arvore::retirar(nodo *raiz, int n){
+    nodo *aux;
+    if(raiz==nullptr){
+        return raiz;
+    }
+    if(n>raiz->info){
+        raiz->dir = retirar(raiz->dir, n);
+    }
+    else if(n<raiz->info){
+        raiz->esq = retirar(raiz->esq, n);
+    }
+    else {
+        if(raiz->esq == nullptr){
+            aux=raiz;
+            raiz=raiz->dir;
+            delete aux;
+            return raiz;
+        }
+        if(raiz->dir==nullptr){
+            aux=raiz;
+            raiz=raiz->esq;
+            delete aux;
+            return raiz;
+        }
+        else{
+            raiz->dir = substituir(raiz, raiz->dir);
+        }
+    }
+    return raiz;
+}
+
+Arvore::listaNodos(){
+
 }
